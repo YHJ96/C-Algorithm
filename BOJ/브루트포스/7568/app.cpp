f7f5efc9a07335a25e8b006702c5addfc7d9b0b1
YHJ96 <yhj878812@naver.com>
2022-09-11 17:40:40 +0900

    해결완료

--- BOJ/브루트포스/7568/app.cpp
+++ BOJ/브루트포스/7568/app.cpp
@@ -1,31 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n;
-int a[200][2];
-int ret[200];
-int ans[200];
+int a[54][2];
+int ret[54];
 
 int main(void) {
-    // freopen("./input.txt", "r", stdin);
+    freopen("./input.txt", "r", stdin);
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     cin >> n;
     for(int i = 0; i < n; i++) {
-        cin >> a[i][0] >> a[i][1];       
+        cin >> a[i][0] >> a[i][1];
     }
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            if (i == j) continue;
-            if (a[i][0] > a[j][0] && a[i][1] > a[j][1]) ret[i]++;
+    // 완전탐색 시작
+    for(int i = 0; i < n - 1; i++) {
+        for(int j = i + 1; j < n; j++){
+            // 완전탐색을 하면서 덩치가 작은 경우 해당되는 사람 + 1
+            if (a[i][0] > a[j][0] && a[i][1] > a[j][1]) ret[j]++;
+            if (a[i][0] < a[j][0] && a[i][1] < a[j][1]) ret[i]++;
         }
     }
+
     for(int i = 0; i < n; i++) {
-        ans[i]++;
-        for(int j = 0; j < n; j++) {
-            if(i == j) continue;
-            if (ret[i] < ret[j]) ans[i]++;
-        }
-        cout << ans[i] << ' ';
+        // 등수 + 1 정제 
+        cout << ret[i] + 1 << " ";
     }
 }
