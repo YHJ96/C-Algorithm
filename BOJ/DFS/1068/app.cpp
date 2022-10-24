30f8a267f6d376769ef137923d26deb46534d970
YHJ96 <yhj878812@naver.com>
2022-10-25 00:10:00 +0900

    해결완료

--- BOJ/DFS/1068/app.cpp
+++ BOJ/DFS/1068/app.cpp
@@ -1,41 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, t;
+int n, target, root;
 vector<int> v[54];
-int visited[54];
-int cnt = 0;
-int root;
 
-void dfs(int node) {
-    visited[node] = 1;
-    if (v[node].size() == 1) cnt++;
+int dfs(int node) {
+    int ret = 0;
+    int child = 0;
     for(auto next: v[node]) {
-        if (visited[next]) continue;
-        dfs(next);
+        if (next == target) continue;
+        ret += dfs(next);
+        // 다음 좌표가 진행되면 child + 1
+        child++;
     }
+    // 해당 노드의 자식이 없으면 1 출력
+    if (child == 0) return 1;
+    return ret;
 }
 
-// 78%
 int main(void) {
     freopen("./input.txt", "r", stdin);
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     cin >> n;
+    // 트리 단방향 그래프 정제
     for(int i = 0; i < n; i++) {
-        int node1 = i, node2; 
-        cin >> node2;
-        if (node2 == -1) {
-            root = i;
-            continue;
-        }
-        v[node1].push_back(node2);
-        v[node2].push_back(node1);
+        int node; cin >> node;
+        if (node == -1) root = i;
+        else v[node].push_back(i);
     }
-
-    cin >> t;
-    visited[t] = 1;
-    dfs(root);
-    if (t == root) cout << 0;
-    else cout << cnt;
+    cin >> target;
+    // 루트 노드 예외 처리
+    if (root == target) cout << 0;
+    else cout << dfs(root);
 }
