8b831beb74bd06e918498cef3e12ac7b8cc30d33
YHJ96 <yhj878812@naver.com>
2022-10-31 11:26:03 +0900

    해결완료

--- BOJ/스택/4949/app.cpp
+++ BOJ/스택/4949/app.cpp
@@ -2,35 +2,49 @@
 using namespace std;
 string s;
 
-// 20%
 int main(void) {
     freopen("./input.txt", "r", stdin);
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
-    while(getline(cin, s)) {
+    while(true) {
+        getline(cin, s);
+        if (s == ".") break;
         stack<char> stk;
         bool flag = false;
+        // 문자열 탐색 시작
         for(auto item: s) {
-            if (item == ')') {
-                if (stk.size() != 0 && stk.top() == '(') stk.pop();
-                else flag = true;
-            }
-
-            if (item == ']') {
-                if (stk.size() != 0 && stk.top() == '[') stk.pop();
-                else flag = true;
-            }
-
+            // 문자가 '('이거나 '['이면 스택에 push
             if (item == '(' || item == '[') stk.push(item);
-        }
+            else {
+                if (item == ')') {
+                    // 괄호의 짝이 맞으면 pop
+                    if (stk.size() != 0 && stk.top() == '(') stk.pop();
+                    // 아닌 경우 정지
+                    else {
+                        flag = true;
+                        break;
+                    }
+                }
 
+                if (item == ']') {
+                    // 괄호의 짝이 맞으면 pop
+                    if (stk.size() != 0 && stk.top() == '[') stk.pop();
+                    // 아닌 경우 정지
+                    else {
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+        }
+        // 정지 예외처리
         if (flag) {
             cout << "no" << "\n";
             continue;
         }
-
-        if (stk.size() != 0) cout << "no" << "\n";
-        else cout << "yes" << "\n";
+        // 스택이 비어있으면 올바른 괄호이며 아닌 경우 no 출력
+        if (stk.empty()) cout << "yes" << "\n";
+        else cout << "no" << "\n";
     }
 }
