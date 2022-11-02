e66e6411fe9bf6b338f783d32b63d5f19488777c
YHJ96 <yhj878812@naver.com>
2022-11-02 12:54:25 +0900

    해결완료

---
+++ BOJ/백트래킹/15649/app.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, m;
+int a[12];
+int visited[12];
+
+void dfs(int depth) {
+    // depth가 진행이 완료되면 종료
+    if (depth == m) {
+        for(int i = 0; i < m; i++) {
+            cout << a[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    // 백트래킹 시작
+    for(int i = 1; i <= n; i++) {
+        if(visited[i]) continue;
+        // 방문 처리
+        visited[i] = 1;
+        // 배열의 깊이에 해당 값 넣기
+        a[depth] = i;
+        // 다음 좌표 진행
+        dfs(depth + 1);
+        // 방문 해제
+        visited[i] = 0;
+    }
+}
+
+int main(void) {
+    freopen("./input.txt", "r", stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> n >> m;
+    dfs(0);
+}
